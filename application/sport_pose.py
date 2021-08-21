92a063d44dac948cad65bdfa60da0f074c46eab3
Fulvio Sanguigni <63920397+furio1999@users.noreply.github.com>
2021-08-21 22:45:47 +0200

    Update sport_pose.py

--- application/sport_pose.py
+++ application/sport_pose.py
@@ -13,9 +13,6 @@ import matplotlib.pyplot as plt
 import HandTracker as Ht
 # %matplotlib inline
 
-#!git clone https://github.com/google/mediapipe.git
-# %cd mediapipe
-# !pip list -v mediapipe
 
 
 
@@ -41,40 +38,7 @@ lmList=pose_photo(img)
 width, height, c=img.shape
 plot(lmList, width, height)
 
-#punta piede verso il basso: poco accurato
-
-## Detect poses on a test video
-
-We are going to detect poses on the following youtube video:
-"""
-
-#https://stackoverflow.com/questions/51143458/difference-in-output-with-waitkey0-and-waitkey1/51143586 problem: it does not show as continuous feed
-#1) highlight the three points
-#2) graphics to visually show angle
-#3) funzione dove input=parte del corpo, output=highlight landmarks su parte del corpo desiderata
-#in teoria, sia da webcam che da video esterno dovrei avere la sequenza di immagini. Perché con yolov4 file no? non è solo cv_imshow il problema. Sembra quasi che 
-#con delle funzioni di supporto javascript riesco ad avere un continuous flow
-#se aggiro il problema cv_imshow mantenendo le funzioni javascript, cambierebbe qualcosa almeno per l'input da webcam? /edit, stesso problema anche con video 
-#senza pose tracker
-#per webcam: funziona usare qualcos'altro invece che cv2_show? tipo PIL?
-#per video salvato: funziona creare un'impalcatura html-js che "aggiri" il ciclo while con dentro cv2_imshow?
-#se faccio cv2_imshow senza fare img=cv2.circle... a cosa è uguale img? resta invariata?
-"""
-le due cose che portano via più tempo: dover aggiornare i salvataggi ed il fatto di non avere continuous stream
-inoltre, sarebbe bello poter modificare il codice mentre l'applicazione gira e poterne vedere gli effetti in real-time, senza dover ri-runnare tutto (a costo di farla inceppare per un comando errato)
-esempio potrei scrivere codice commentato mentre l'applicazione gira, e poi scommentarlo quando sono certo che non dà errori, oppure se devo modificare
-solo una cosa lo posso fare al volo senza conseguenze
-"""
-#"""
 # video_path = "1500_doha.mp4"
-
-# mp4 = open(video_path, "rb").read()
-# data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-# HTML(f"""
-# <video width=400 controls>
-#       <source src="{data_url}" type="video/mp4">
-# </video>
-# """)
 
 def search(list, platform):
     for i in range(len(list)):
